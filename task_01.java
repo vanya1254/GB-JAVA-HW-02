573bb1ec20dc223f989d5f603c67e700e595c569
Ivan <vanya1254@yandex.ru>
2023-02-18 13:43:25 +0300

    add code from app.java to task_01

--- task_01.java
+++ task_01.java
@@ -1,3 +1,12 @@
+// package com.example;
+
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 /**
  * Дана строка sql-запроса "select * from students where ". Сформируйте часть
  * WHERE этого запроса, используя StringBuilder. Данные для фильтрации приведены
@@ -9,8 +18,46 @@
  * "city":"Moscow", "age":"null"}
  */
 
-public class task_01 {
+public class App {
     public static void main(String[] args) {
-        // Object obj = new JSONParser().parse(new FileReader("01.json"));
+
+        Map<String, String> dictionary = new HashMap<String, String>();
+
+        JSONParser parser = new JSONParser();
+
+        try (FileReader reader = new FileReader("01.json");) {
+
+            JSONObject rootJsonObject = (JSONObject) parser.parse(reader);
+
+            String name = (String) rootJsonObject.get("name");
+            String country = (String) rootJsonObject.get("country");
+            String city = (String) rootJsonObject.get("city");
+            String age = (String) rootJsonObject.get("age");
+
+            dictionary.put("name", name);
+            dictionary.put("country", country);
+            dictionary.put("city", city);
+            dictionary.put("age", age);
+
+            StringBuilder sql = new StringBuilder("SELECT * FROM students WHERE");
+
+            for (Map.Entry<String, String> entry : dictionary.entrySet()) {
+                String key = entry.getKey().toString();
+                String value = entry.getValue().toString();
+                if (value.equals("null"))
+                    continue;
+                else {
+                    if (key.equals("country"))
+                        sql.append(" ").append(key).append(" = ").append(value);
+                    else
+                        sql.append(" AND ").append(key).append(" = ").append(value);
+                }
+            }
+
+            System.out.println(sql.toString());                                                     // SELECT * FROM students WHERE country = Russia AND city = Moscow AND name = Ivanov
+
+        } catch (Exception e) {
+            System.out.println("Parsing error" + e.toString());
+        }
     }
 }
